7caf8dfdca0c946010be0c6bed88cb01b7172160
NATALIA <66316060+natsnegireva@users.noreply.github.com>
2022-01-11 17:07:50 +0300

    Домашнее задание 2 spring-2

--- src/main/resources/static/orders/orders.js
+++ src/main/resources/static/orders/orders.js
@@ -1,6 +1,5 @@
 angular.module('app').controller('ordersController', function ($scope, $http) {
     const contextPath = 'http://localhost:8189/watch';
-
     $scope.showMyOrders = function () {
         $http({
             url: contextPath + '/api/v1/orders',
